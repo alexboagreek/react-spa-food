d9bbada912bc23189a283b75d483afe03ac5eafe
Alex <olivesargreek@gmail.com>
2022-02-01 16:06:21 +0300

    feat: categoryItem

--- react-spa-food/src/components/CategoryItem.jsx
+++ react-spa-food/src/components/CategoryItem.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 function CategoryItem(props) {
     const { idCategory, strCategory, strCategoryThumb, strCategoryDescription } = props;
     return <div className="card">
-        <div class="card">
-            <div class="card-image">
+        <div className="card">
+            <div className="card-image">
             <img src={ strCategoryThumb } alt={ strCategory }/>
             
            
         </div>
-        <div class="card-content">
+        <div className="card-content">
           <p>{ strCategoryDescription.slice(0, 60) }...</p>
-          <span class="card-title">{ strCategory }</span>
+          <span className="card-title">{ strCategory }</span>
         </div>
         <div className="card-action">
             <Link to={`/category/${idCategory}`} className="btn">Watch category</Link>
