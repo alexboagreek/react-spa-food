d7dfe9de2404d28e96d71c3c97d484cc3cbfdb41
Alex <olivesargreek@gmail.com>
2022-02-02 07:00:02 +0300

    fix: bug into API_URL

--- react-spa-food/src/config.js
+++ react-spa-food/src/config.js
@@ -3,6 +3,6 @@ const API_KEY = '1';
 
 /* базовый адрес */
 
-const API_URL = `www.themealdb.com/api/json/v1/${API_KEY}/`;
+const API_URL = `https://www.themealdb.com/api/json/v1/${API_KEY}/`;
 
 export { API_URL };
